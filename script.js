a084412fb5ff4d624fea76fa547bc02467441e25
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-12-18 09:15:50 +0000

    Final commit - create event handlers changing color of buttons.

--- script.js
+++ script.js
@@ -2,3 +2,19 @@
 const pickBtn = document.getElementById("color-button");
 //"Mystery Color" button
 const nextBtn = document.getElementById("next-button");
+
+//random color function:
+function colorValue() {
+  const color = Math.floor(Math.random() * 256);
+  return color;
+}
+
+function colorChange(e) {
+  const randomColor = `rgb(${colorValue()},${colorValue()},${colorValue()})`;
+
+  e.target.style.backgroundColor = randomColor;
+  // console.log(e);
+}
+
+pickBtn.onclick = colorChange;
+nextBtn.onwheel = colorChange;
